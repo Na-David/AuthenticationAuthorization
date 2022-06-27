4f5f6b918d00cd59ca982c717a7ef77f1fd20d67
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-28 00:52:51 +1200

    Claim Instance Practice

---
+++ Auth/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Auth
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    string content = "<h1>ASP.NET Core Authorization & Authentication æ»≥Á«œººø‰</h1>";
+                    context.Response.Headers["Content-Type"] = "text/html; charset = utf-8";
+                    await context.Response.WriteAsync(content);
+                });
+
+                endpoints.MapGet("/Login", async context =>
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Aaron")
+                    };
+
+                    context.Response.Headers["Content-Type"] = "text/html; charset = utf-8";
+                    await context.Response.WriteAsync("<h3>Login Successful</h3>");
+                });
+            });
+        }
+    }
+}
